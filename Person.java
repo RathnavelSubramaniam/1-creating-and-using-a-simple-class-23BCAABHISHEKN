d628450372ba367b351de3db31851e373d607738
23BCAABHISHEKN <139528289+23BCAABHISHEKN@users.noreply.github.com>
2024-01-30 05:01:51 +0000

    abhi1

--- Person.java
+++ Person.java
@@ -20,9 +20,9 @@ public class Person
     {
         Person1 a1 = new Person1("Jhon","Doe",30);
         Person1 a2 = new Person1("Alice","Smith",25);
-        System.out.println("Person 1 : "+a1.getfullname());
-        System.out.println("Person 2 : "+a2.getfullname());
+        System.out.println("Person 1:"+a1.getfullname());
+        System.out.println("Person 2:"+a2.getfullname());
         double b = (a1.age+a2.age)/2;
-        System.out.println("Average Age : "+b);
+        System.out.println("Average Age:"+b);
     }
 }
