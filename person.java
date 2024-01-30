3135237b1dc0e866a9dcd5f0c1c01c5ea9ebeeda
23BCAABHISHEKN <139528289+23BCAABHISHEKN@users.noreply.github.com>
2024-01-30 04:49:13 +0000

    abhi1

---
+++ person.java
@@ -0,0 +1,28 @@
+class person1
+{
+    String firstname;
+    String lastname;
+    double age;
+    person1(String fn, String ln,double a)
+    {
+        firstname= fn;
+        lastname= ln;
+        age=a;
+    }
+    String getfullname()
+    {
+        return firstname+" "+lastname;
+    }
+}
+public class person 
+{
+    public static void main (String args [])
+    {
+        person1 a1 = new person1("Jhon","Doe",30);
+        person1 a2 = new person1("Alice","Smith",25);
+        System.out.println("person1:"+a1.getfullname());
+        System.out.println("person2:"+a2.getfullname());
+        double b = (a1.age+a2.age)/2;
+        System.out.println("Average Age:"+b);
+    }
+}
